cc28a11d1c33636bb94bf10c43a76b9324985edd
Eltonite <eltoniteonblockchain@gmail.com>
2022-02-04 00:19:39 -0600

    Install basic Next.js template

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-slate-600">
+      <Head>
+        <title>Defi Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+
+    </div>
+  )
+}
