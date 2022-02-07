7d6af32e3986679cf2b679525ea690a6a31f6c28
Eltonite <eltoniteonblockchain@gmail.com>
2022-02-06 20:48:59 -0600

    Modify _app.tsx to .jsx to bypass moralis issue

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
 
   const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
 
   const appID = process.env.NEXT_PUBLIC_APP_ID
 
-
   return(
-    <MoralisProvider serverUrl={serverURL} appId={appID} >
+    <MoralisProvider serverUrl={serverURL} appId={appID}>
       {
         <Component {...pageProps} />
       }
